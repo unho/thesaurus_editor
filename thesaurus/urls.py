e11d475f3214b7e37ca5bfe80150b39aba5cc65a
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-02-01 19:39:30 +0100

    Added pagination for word list

--- thesaurus/urls.py
+++ thesaurus/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('thesaurus.views',
     url(r'^$', ListView.as_view(
         model=Word,
         context_object_name = "words",
-        #paginate_by = 3,
+        paginate_by = 100,
         template_name="word_list.html",
     ), name='thesaurus_home'),
     url(r'^words/(?P<pk>[\w\ \-,/!]+)/$', 'word_details', name='thesaurus_word'),
